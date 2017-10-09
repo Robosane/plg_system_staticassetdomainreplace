a31aa7172c699047007d336323cd19f0a2313098
Ben Klein <robobenklein@gmail.com>
2017-10-09 13:14:31 -0400

    Style urls

--- staticassetdomainreplace.php
+++ staticassetdomainreplace.php
@@ -5,6 +5,7 @@ defined( '_JEXEC' ) or die;
 class plgSystemStaticAssetDomainReplace extends JPlugin
 {
 	protected $domaininsert = "";
+	protected $r_filetypes;
 
 	// automatically get application instance
 	protected $app;
@@ -20,32 +21,39 @@ class plgSystemStaticAssetDomainReplace extends JPlugin
 		 * $this->app and $this->params respectively
 		 */
 
-		$this->app->enqueueMessage('AfterRender!', 'error');
-		$html = $this->app->getBody();
-		$html = $html . '<!-- lol changed -->';
-
 		if ($this->app->isClient('site')) {
-			$domaininsert = $this->params->get('domain_scheme') . $this->params->get('insert_domain');
+			$html = $this->app->getBody();
+			$this->domaininsert = $this->params->get('domain_scheme') . $this->params->get('insert_domain');
 
-			$html = $this->replaceEnabled($html, $this->params);
+			$this->r_filetypes =
+				str_replace(',', '|', // replace the commas with the regex OR|
+				preg_quote( // puts the \escape for min\.js etc.
+				preg_replace('/\s+/', '', // strip all whitespace
+					$this->params->get('filetypes')
+				)));
 
-			if (empty($html)) {
-				// error some stuff
-				$this->app->enqueueMessage(JText::_('SADM ERROR!'), 'error');
-			} else {
+			$html_new = $this->replaceEnabled($html, $this->params);
+
+			if (empty($html_new)) {
+				// error some stuff?
+				// $this->app->enqueueMessage(JText::_('SADM ERROR!'), 'error');
+			} else { // then it probably worked
+				$html = $html_new;
+				// done, apply changes.
+				$this->app->setBody($html);
 			}
+
 		}
-		else {
-			$this->app->enqueueMessage('SADM: NOT SITE!', 'error');
+		else { // then we're not at the frontend
+			// don't do anything...
 		}
-		$this->app->setBody($html);
 	}
 
 	/*
 	 * Call the enabled replace functions, according to which are enabled
 	 * via the plugin settings.
 	 */
-	protected static function replaceEnabled($html, $plg_params) {
+	protected function replaceEnabled($html, $plg_params) {
 
 		// <link> tags
 		if ($plg_params->get('enable_tag_link')) {
@@ -59,7 +67,7 @@ class plgSystemStaticAssetDomainReplace extends JPlugin
 
 		// <script> tags
 		/*
-		 * Regex: ((?:<script[^>]+(?:src)\s*=\s*)(?!['"]?(?:data|http|\/\/))['"]?)(([^'"\)\s>]+\/([^'"\)\s>\.]+\.(pack\.js|min\.js|js|jpg|png|gif|bmp|ico)))\??[^'"\)\s>]*)
+		 * Regex: ((?:<script[^>]+(?:src)\s*=\s*)(?!['"]?(?:data|http|\/\/))['"]?)((\/[^'"\)\s>]+\/([^'"\)\s>\.]+\.(pack\.js|min\.js|js|jpg|png|gif|bmp|ico)))\??[^'"\)\s>]*)
 		 * use: $1<insert>$2
 		 * source: $2
 		 * no querystring: $3
@@ -67,15 +75,26 @@ class plgSystemStaticAssetDomainReplace extends JPlugin
 		 * file extension: $5
 		 */
 		if ($plg_params->get('enable_tag_script')) {
-			$pattern = '/((?:<script[^>]+(?:src)\s*=\s*)(?![\'"]?(?:data|http|\/\/))[\'"]?)(([^\'"\)\s>]+\/([^\'"\)\s>\.]+\.(pack\.js|min\.js|js|jpg|png|gif|bmp|ico)))\??[^\'"\)\s>]*)/';
-			$replacement = '$1' . $domaininsert . '$2';
+			$pattern = '/((?:<script[^>]+(?:src)\s*=\s*)(?![\'"]?(?:data|http|\/\/))[\'"]?)((\/[^\'"\)\s>]+\/([^\'"\)\s>\.]+\.(' . $this->r_filetypes . ')))\??[^\'"\)\s>]*)/';
+			$replacement = '${1}' . $this->domaininsert . '${2}';
 
 			$html = preg_replace($pattern, $replacement, $html);
 		}
 
 		// url()s in <style> tags
+		/*
+		 * Regex: ((?:url\()(?!['"]?(?:data|http))['"]?)(([^'"\)\s>]+\/([^'"\)\s>]+\.(?:js|jpg|png|gif|bmp|ico)))[^[:alnum:]]\??[^'"\)\s>]*)
+		 * use: $1<insert>$2
+		 * source: $2
+		 * no querystring: $3
+		 * filename without path: $4
+		 * file extension: $5
+		 */
 		if ($plg_params->get('enable_tag_style_url')) {
+			$pattern = '/((?:url\()(?![\'"]?(?:data|http))[\'"]?)(([^\'"\)\s>]+\/([^\'"\)\s>]+\.(?:' . $this->r_filetypes . ')))[^[:alnum:]]\??[^\'"\)\s>]*)/';
+			$replacement = '${1}' . $this->domaininsert . '${2}';
 
+			$html = preg_replace($pattern, $replacement, $html);
 		}
 
 		return $html;
